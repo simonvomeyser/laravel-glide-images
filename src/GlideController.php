dfa6c9061912aa323fc5f2c0766896d9b63bf675
Simon vom Eyser <simon.vom.eyser@gmail.com>
2024-12-11 08:02:07 +0100

    feat: update for use with laravel 11.x, php 8.3
    Remove glide laravel dep
    
    See https://github.com/thephpleague/glide-laravel/issues/11

--- src/GlideController.php
+++ src/GlideController.php
@@ -4,7 +4,7 @@ namespace SimonVomEyser\LaravelGlideImages;
 
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Storage;
-use League\Glide\Responses\LaravelResponseFactory;
+use League\Glide\Responses\SymfonyResponseFactory;
 use League\Glide\ServerFactory;
 use League\Glide\Signatures\SignatureException;
 use League\Glide\Signatures\SignatureFactory;
@@ -18,7 +18,7 @@ class GlideController
         $source = Storage::disk('glide_public_path')->path('');
 
         $server = ServerFactory::create([
-            'response' => new LaravelResponseFactory(app('request')),
+            'response' => new SymfonyResponseFactory(app('request')),
             'source' => $source,
             'cache' => $filesystem->getDriver(),
             'cache_path_prefix' => config('glide-images.cache'),
